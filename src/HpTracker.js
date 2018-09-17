73b7c392bbb622b7720582955976697c59a87a5f
Miikka Alatalo <alatalo.miikka@gmail.com>
2018-09-17 16:31:35 +0300

    ¯\_(ツ)_/¯

--- src/HpTracker.js
+++ src/HpTracker.js
@@ -1,122 +1,121 @@
 import React, { Component } from 'react';
-import './App.css';
+import './HpTracker.css';
 import { withStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
-
 import ToggleButton from '@material-ui/lab/ToggleButton';
 import ToggleButtonGroup from '@material-ui/lab/ToggleButtonGroup';
 
 import PlayerBox from './PlayerBox';
 
 const styles = theme => ({
-  root: {
-    flex: 1,
-    backgroundColor: "#E9573F",
-    height: "100%",
-  },
-  bigbox: {
-    padding: 10,
-    height: "100%",
-  },
-  splitbox: {
-  },
-  containerbox: {
-    height: "100%",
-  },
+    root: {
+        flex: 1,
+        height: "100%",
+    },
+    bigbox: {
+        padding: 10,
+        height: "100%",
+    },
+    containerbox: {
+        height: "100%",
+    },
 });
 
 
 
 class HpTracker extends Component {
 
-  componentWillMount = () => {
-    this.setState({
-      players: [
-        { "hp": 21 },
-        { "hp": 22 },
-        { "hp": 23 },
-        { "hp": 24 },
-      ]
-    })
-    this.handleSelection(null, "2v");
-  }
-
-  handleSelection = (event, sel) => {
-    if (sel === null) return;
-    const select = sel.split("");
-    const playerCount = parseInt(select[0], 10);
-    const playerWidth = select[1] === "v" ? 12 : select[1] === "h" && playerCount === 4 ? 3 : 6;
-    let paperHeight = "98%";
-
-    if (playerCount === 4 && select[1] === "g") {
-      paperHeight = "48%";
-    } else if (playerCount === 2 && playerWidth === 6) {
-      paperHeight = "98%";
-    } else if (playerCount === 4 && playerWidth === 6) {
-      paperHeight = "48%";
-    } else if (playerCount === 2 && playerWidth === 12) {
-      paperHeight = "48%";
-    } else if (playerCount === 4 && playerWidth === 12) {
-      paperHeight = "23%";
+    componentWillMount = () => {
+        this.setState({
+            players: [
+                { "hp": 21 },
+                { "hp": 22 },
+                { "hp": 23 },
+                { "hp": 24 },
+            ]
+        })
+        this.handleSelection(null, { p: 2, d: "v" });
     }
 
-    this.setState({
-      selection: sel,
-      playerWidth: playerWidth,
-      playerCount: playerCount,
-      paperHeight: paperHeight,
-    });
-  }
+    handleSelection = (event, sel) => {
+        if (sel === null) return;
+        const playerWidth = sel.d === "v" ? 12 : sel.d === "h" && sel.p === 4 ? 3 : 6;
+        let paperHeight = "98%";
 
-  handleHPChange = (id, amount) => {
-    let players = [ ...this.state.players ];
-    players[id] = {...players[id], hp: players[id].hp+amount};
-    this.setState({ players });
-  }
+        if (sel.p === 4 && sel.d === "g") {
+            paperHeight = "48%";
+        } else if (sel.p === 2 && playerWidth === 6) {
+            paperHeight = "98%";
+        } else if (sel.p === 4 && playerWidth === 6) {
+            paperHeight = "48%";
+        } else if (sel.p === 2 && playerWidth === 12) {
+            paperHeight = "48%";
+        } else if (sel.p === 4 && playerWidth === 12) {
+            paperHeight = "23%";
+        }
 
-  render() {
-    const { classes } = this.props;
+        this.setState({
+            selection: sel,
+            playerWidth: playerWidth,
+            playerCount: sel.p,
+            paperHeight: paperHeight,
+            direction: sel.d,
+        });
+    }
 
-    return (
-      <div className={classes.root}>
-        <Grid container className={classes.bigbox}>
-          <Grid item xs={12}>
-            <Grid container spacing={16} className={classes.containerbox} alignItems={"center"} justify={"space-evenly"}>
-              {[...Array(this.state.playerCount)].map((val, i) => (
-                <PlayerBox id={i} 
-                  player={this.state.players[i]} 
-                  paperHeight={this.state.paperHeight} 
-                  playerWidth={this.state.playerWidth} 
-                  handleHPChange={this.handleHPChange} />
-              ))}
-            </Grid>
-          </Grid>
-        </Grid>
+    handleHPChange = (id, amount) => {
+        let players = [...this.state.players];
+        players[id] = { ...players[id], hp: players[id].hp + amount };
+        this.setState({ players });
+    }
 
-        {/* Placeholder selector */}
-        <div className={classes.toggleContainer}>
-          <ToggleButtonGroup value={this.state.selection} exclusive onChange={this.handleSelection}>
-            <ToggleButton value="2h">
-              2h
-            </ToggleButton>
-            <ToggleButton value="4h">
-              4h
-            </ToggleButton>
-            <ToggleButton value="2v">
-              2v
-            </ToggleButton>
-            <ToggleButton value="4v">
-              4v
-            </ToggleButton>
-            <ToggleButton value="4g">
-              4g
-            </ToggleButton>
-          </ToggleButtonGroup>
-        </div>
-      </div>
-    );
-  }
+    render() {
+        const { classes } = this.props;
+
+        return (
+            <div className={classes.root}>
+                <Grid container className={classes.bigbox}>
+                    <Grid item xs={12}>
+                        <Grid container spacing={16} className={classes.containerbox} alignItems={"center"} justify={"space-evenly"}>
+                            {[...Array(this.state.playerCount)].map((val, i) => (
+                                <PlayerBox key={i}
+                                    id={i}
+                                    player={this.state.players[i]}
+                                    paperHeight={this.state.paperHeight}
+                                    playerWidth={this.state.playerWidth}
+                                    handleHPChange={this.handleHPChange}
+                                    direction={this.state.direction} />
+                            ))}
+                        </Grid>
+                    </Grid>
+                </Grid>
+
+                {/* Placeholder selector */}
+                <div className={classes.toggleContainer}>
+                    <ToggleButtonGroup value={this.state.selection} exclusive onChange={this.handleSelection}>
+                        <ToggleButton value={{ p: 2, d: "h" }}>
+                            2 horizontal
+                        </ToggleButton>
+                        <ToggleButton value={{ p: 4, d: "h" }}>
+                            4 horizontal
+                        </ToggleButton>
+                        <ToggleButton value={{ p: 2, d: "v" }}>
+                            2 vertical
+                         </ToggleButton>
+                        <ToggleButton value={{ p: 4, d: "v" }}>
+                            4 vertical
+                        </ToggleButton>
+                        <ToggleButton value={{ p: 4, d: "g" }}>
+                            4 h-grid
+                        </ToggleButton>
+                        <ToggleButton value={{ p: 4, d: "c" }}>
+                            4 v-grid
+                        </ToggleButton>
+                    </ToggleButtonGroup>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default withStyles(styles)(HpTracker);
--- src/PlayerBox.js
+++ src/PlayerBox.js
@@ -1,29 +1,81 @@
 import React, { Component } from 'react';
-import './App.css';
 import { withStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import AddIcon from '@material-ui/icons/Add';
+import RemoveIcon from '@material-ui/icons/Remove';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import OpacityIcon from '@material-ui/icons/Opacity';
+import WhatsHotIcon from '@material-ui/icons/Whatshot';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import red from '@material-ui/core/colors/red';
+import green from '@material-ui/core/colors/green';
 
 const styles = theme => ({
     paper: {
         padding: 10,
         textAlign: 'center',
-        color: theme.palette.text.secondary,
         height: "100%",
+        backgroundColor: "rgba(255, 255, 255, .2)",
+    },
+    button: {
+        margin: theme.spacing.unit,
+    },
+    container: {
+        height: "100%",
+    },
+    addButton: {
+        filter: "opacity(80%)",
+    },
+    removeButton: {
+        filter: "opacity(80%)",
+    },
+    text: {
+        color: "#212121",
+        textShadow: "0px 0px 15px white",
+    }
+});
+
+const theme = createMuiTheme({
+    palette: {
+        primary: green,
+        secondary: red,
     },
 });
 
 class PlayerBox extends Component {
     render() {
         const { classes } = this.props;
-
+        let ddirection = this.props.direction === "c" || this.props.direction ==="h" ? "column-reverse" : "row" ;
         return (
-            <Grid key={this.props.id} item xs={this.props.playerWidth} style={{ height: this.props.paperHeight }}>
+            <Grid item xs={this.props.playerWidth} style={{ height: this.props.paperHeight }}>
                 <Paper className={classes.paper} style={{ paddingTop: 10, paddingBottom: 10, height: "100%" }}>
-                    {`Player ${this.props.id}`}<br /><br /><br />
-                    <button onClick={()=>this.props.handleHPChange(this.props.id,-1)}>-</button>
-                    {`HP ${this.props.player.hp}`}
-                    <button onClick={()=>this.props.handleHPChange(this.props.id,1)}>+</button>
+                    <Grid container direction={ddirection} justify="center" alignItems="center" className={classes.container}>
+                        <MuiThemeProvider theme={theme}>
+                            <Grid container justify="center" alignItems="center" xs={4}>
+                                <Button variant="fab" aria-label="Add" color="secondary"
+                                    onClick={() => this.props.handleHPChange(this.props.id, -1)} className={classes.addButton}>
+                                    { /* <RemoveIcon />
+                                    <WhatsHotIcon */ }
+                                    <OpacityIcon />
+                                </Button>
+                            </Grid>
+                            <Grid container justify="center" alignItems="center" xs={4}>
+                                <Typography variant="display4" className={classes.text} style={{ margin: 0 }}>
+                                    {this.props.player.hp}
+                                </Typography>
+                            </Grid>
+                            <Grid container justify="center" alignItems="center" xs={4}>
+                                <Button variant="fab" aria-label="Add" color="primary"
+                                    onClick={() => this.props.handleHPChange(this.props.id, 1)} className={classes.removeButton}>
+                                    { /* <AddIcon /> */}
+                                    <FavoriteIcon />
+                                </Button>
+                            </Grid>
+                        </MuiThemeProvider>
+                    </Grid>
                 </Paper>
             </Grid>
         )
