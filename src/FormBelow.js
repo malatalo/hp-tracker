9e3d3935e5a7ee55a3075c6e61fe84e55865301a
Miikka Alatalo <alatalo.miikka@gmail.com>
2018-09-21 15:10:29 +0300

    fix textfield width

--- src/FormBelow.js
+++ src/FormBelow.js
@@ -30,7 +30,7 @@ const styles = theme => ({
     textField: {
         marginLeft: theme.spacing.unit,
         marginRight: theme.spacing.unit,
-        width: 200,
+        width: 150,
     },
     select: {
         width: 150,
